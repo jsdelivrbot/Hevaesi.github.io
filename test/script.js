a8e2623d94d00e96df7fb87e8caafc40cc428eaf
Hevaesi <jaidy379@gmail.com>
2017-11-09 16:49:36 +0200

    Update script.js

--- test/script.js
+++ test/script.js
@@ -26,6 +26,7 @@ function enableCamera() {
         ce.video.srcObject = stream;
         ce.video.onloadedmetadata = () => {
             ce.video.play();
+            drawOnCanvas();
         }
     })
     .catch((err) => {
